d4ca0c4f7be345c9a88a4cc5ca12913e5bf040eb
TuChief <gtuchief@gmail.com>
2023-04-17 16:33:11 +0800

    Update suying_checkin.py

--- suying_checkin.py
+++ suying_checkin.py
@@ -4,23 +4,27 @@ import time, os
 from selenium.webdriver.support.ui import WebDriverWait
 import requests
 from urllib import parse
-from check_url import get_valid_url,update_urls
+from check_url import get_valid_url, update_urls
+from selenium.webdriver.common.by import By
 
 os.system("killall -9 chrome")
 os.system("killall -9 chromedriver")
 
 # ============== 以下内容按需修改
 # 登录邮箱
-email = "邮箱"
+email = "登录邮箱"
 # 登录密码
-password = "密码"
+password = "登录密码"
 # 通知标题
-notice_title = "通知标题"
+notice_title = "速鹰666签到"
 # Bark服务推送地址，可以更换为 PushDeer 或者 Server酱
 notice_objects = [
-    "http://api.day.app/token1"
-    ,"http://api.day.app/token2"
-    ,"http://api.day.app/token3"
+    # myself
+    "https://api.day.app/token1"
+    # 郭伟斌
+    ,"https://api.day.app/token2"
+    # 郭伟斌朋友 咔咔、
+    ,"https://api.day.app/token3"
 ]
 # 推送公告内容最大字数，超过部分用...表示
 notice_words_max = 200
@@ -41,39 +45,39 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--window-size=1920,1080')
-driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
+driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
 
 print("初始化浏览器完成，正在打开页面 " + page_url)
 driver.get(page_url)
 
 # 1、登录界面
-driver.implicitly_wait(15)
+driver.implicitly_wait(60)
 print("——" +driver.title)
 # 输入邮箱
 time.sleep(1)
 print('————输入邮箱')
-driver.find_element_by_id('email').send_keys(email)
+driver.find_element(By.ID,'email').send_keys(email)
 # 输入密码
 time.sleep(1)
 print('————输入密码')
-driver.find_element_by_id('password').send_keys(password)
+driver.find_element(By.ID,'password').send_keys(password)
 # 点击登录
 time.sleep(1)
 print('————点击登录')
-driver.find_element_by_class_name('login').click()
+driver.find_element(By.CLASS_NAME,'login').click()
 
 # 2、签到
 
 print("——等待登录完成")
-driver.implicitly_wait(15)
+driver.implicitly_wait(60)
 
 print("——登录完成，开始签到")
 # 登录完成，等待页面加载完成
-WebDriverWait(driver, 15).until(lambda driver: driver.find_element_by_id("checkin-div"))
+WebDriverWait(driver, 60).until(lambda driver: driver.find_element(By.ID,"checkin-div"))
 # 刷新当前页面，避免页面出现公告弹出框而导致后续操作无法继续（刷新后公告弹出框当天不会再次出现）
 driver.refresh()
 # 今日是否已签到
-button_text = driver.find_element_by_id('checkin-div').text
+button_text = driver.find_element(By.ID,'checkin-div').text
 # 签到结果
 check_result = ''
 if button_text == '明日再来':
@@ -81,33 +85,33 @@ if button_text == '明日再来':
     check_result = 'ㄟ(▔,▔)ㄏ 今日已签到，明日再来'
 else:
     print("————点击签到按钮")
-    driver.find_element_by_id('checkin-div').click()
-    WebDriverWait(driver, 15).until(lambda driver: driver.find_element_by_id("swal2-content"))
+    driver.find_element(By.ID,'checkin-div').click()
+    WebDriverWait(driver, 60).until(lambda driver: driver.find_element(By.ID,"swal2-content"))
     time.sleep(1)
-    check_result = "╰(￣▽￣)╭" + driver.find_element_by_id('swal2-content').text
+    check_result = "╰(￣▽￣)╭" + driver.find_element(By.ID,'swal2-content').text
     time.sleep(2)
-    driver.find_element_by_xpath('/html/body/div[4]/div/div[3]/button[1]').click()
+    driver.find_element(By.XPATH,'/html/body/div[4]/div/div[3]/button[1]').click()
 
 # 3、会员相关信息
 
 print('——获取会员相关信息')
 time.sleep(5)
 # 会员剩余时长
-vip_remaining_time = driver.find_element_by_xpath('//*[@id="app"]/div/div[3]/section/div[2]/div[1]/div/div[2]/div[2]/span').text
+vip_remaining_time = driver.find_element(By.XPATH,'//*[@id="app"]/div/div[3]/section/div[2]/div[1]/div/div[2]/div[2]/span').text
 print("————会员剩余时长：" + vip_remaining_time)
 
 # 剩余流量
-residual_flow_and_unit = driver.find_element_by_xpath('//*[@id="app"]/div/div[3]/section/div[2]/div[2]/div/div[2]/div[2]').text
+residual_flow_and_unit = driver.find_element(By.XPATH,'//*[@id="app"]/div/div[3]/section/div[2]/div[2]/div/div[2]/div[2]').text
 print("————剩余流量：" + residual_flow_and_unit)
 
 # 在线设备
-current_connected = driver.find_element_by_xpath('//*[@id="app"]/div/div[3]/section/div[2]/div[3]/div/div[2]/div[2]/span[1]').text
-sum_connect = driver.find_element_by_xpath('//*[@id="app"]/div/div[3]/section/div[2]/div[3]/div/div[2]/div[2]/span[2]').text
+current_connected = driver.find_element(By.XPATH,'//*[@id="app"]/div/div[3]/section/div[2]/div[3]/div/div[2]/div[2]/span[1]').text
+sum_connect = driver.find_element(By.XPATH,'//*[@id="app"]/div/div[3]/section/div[2]/div[3]/div/div[2]/div[2]/span[2]').text
 print("————在线设备：" + current_connected + "/" + sum_connect)
 
 # 公告
 print('——公告')
-notice = driver.find_element_by_xpath('//*[@id="app"]/div/div[3]/section/div[3]/div[1]/div/div[2]').text
+notice = driver.find_element(By.XPATH,'//*[@id="app"]/div/div[3]/section/div[3]/div[1]/div/div[2]').text
 notice_len = len(notice)
 if notice_len > notice_words_max:
     notice = notice[0:notice_words_max] + "..."
@@ -129,7 +133,7 @@ for notice_object in notice_objects:
 # 获取备用网址，更新到urls.txt文件中
 print('——更新URL地址文件')
 urls = []
-url_list_el = driver.find_elements_by_xpath("//*[contains(@href,'https://suying')]")
+url_list_el = driver.find_elements(By.XPATH,"//*[contains(@href,'https://suying')]")
 for url_el in url_list_el:
     urls.append(url_el.text)
 # 更新到url文件中
@@ -137,3 +141,7 @@ update_urls(urls)
 
 time.sleep(3)
 driver.quit()
+
+
+
+
